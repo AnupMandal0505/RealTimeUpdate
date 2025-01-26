51e9eae8e2942f45dde3d0f64afede408cbafcb7
anurag <anurag@gmail.com>
2025-01-27 00:35:47 +0530

    s

--- backend/settings.py
+++ backend/settings.py
@@ -27,19 +27,60 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['realtimeupdate.onrender.com', '127.0.0.1']
 
-# Redis Configuration
-REDIS_URL = 'rediss://red-cuagigtsvqrc73doni7g:D2Qc2hvyjF3yOG49tdWRMT8D4C8yff3P@oregon-redis.render.com:6379'
+import redis
 
-# Cache configuration
+# Redis connection settings
+REDIS_HOST = 'oregon-redis.render.com'
+REDIS_PORT = 6379
+REDIS_USERNAME = 'red-cuagigtsvqrc73doni7g'
+REDIS_PASSWORD = 'D2Qc2hvyjF3yOG49tdWRMT8D4C8yff3P'
+REDIS_TLS = True
+
+# Redis client ko create karein
+redis_client = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    username=REDIS_USERNAME,
+    password=REDIS_PASSWORD,
+    ssl=REDIS_TLS
+)
+
+# Redis client ko use karein
+redis_client.set('key', 'value')
+value = redis_client.get('key')
+print(value)
+
+
+# settings.py
+REDIS_HOST = 'oregon-redis.render.com'
+REDIS_PORT = 6379
+REDIS_USERNAME = 'red-cuagigtsvqrc73doni7g'
+REDIS_PASSWORD = 'D2Qc2hvyjF3yOG49tdWRMT8D4C8yff3P'
+REDIS_TLS = True
+
 CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": REDIS_URL,
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': f'redis://{REDIS_USERNAME}:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'SSL': REDIS_TLS
         }
     }
 }
+# # Redis Configuration
+# REDIS_URL = 'rediss://red-cuagigtsvqrc73doni7g:D2Qc2hvyjF3yOG49tdWRMT8D4C8yff3P@oregon-redis.render.com:6379'
+
+# # Cache configuration
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": REDIS_URL,
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 # Application definition
 
@@ -157,14 +198,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ASGI_APPLICATION = 'backend.asgi.application'
 
 # Channel layers configuration for WebSocket
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [REDIS_URL],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "channels_redis.core.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [REDIS_URL],
+#         },
+#     },
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
